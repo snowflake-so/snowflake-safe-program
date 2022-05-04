95f5c08f016ee776e691ad637b4660f6df070feb
Quang Mai <quang@snowflake.so>
2022-05-04 13:56:15 +0700

    Update endpoints for the node operator

--- snowflake-rust-v2/programs/snowflake/src/instructions/execute_scheduled_multisig_flow.rs
+++ snowflake-rust-v2/programs/snowflake/src/instructions/execute_scheduled_multisig_flow.rs
@@ -2,27 +2,33 @@ use anchor_lang::prelude::*;
 
 use crate::common::charge_fee;
 use crate::error::ErrorCode;
-use crate::instructions::{do_execute_multisig_flow, ExecuteMultisigFlow};
+use crate::instructions::{
+    do_execute_multisig_flow, validate_multisig_flow_before_execute, ExecuteMultisigFlow,
+};
 use crate::state::static_config::ProposalStateType;
 
 pub fn handler<'info>(ctx: Context<ExecuteMultisigFlow>) -> Result<()> {
+    validate_multisig_flow_before_execute(&ctx)?;
     charge_fee(&ctx)?;
 
     let now = Clock::get()?.unix_timestamp;
     let flow = &ctx.accounts.flow;
-    
     // let operator = &ctx.accounts.caller;
     // require!(program_settings.can_operator_excecute_flow(&flow.key(), operator.key), ErrorCode::JobIsNotAssignedToOperator);
-    
-    require!(flow.is_due_for_execute(now), ErrorCode::JobIsNotDueForExecution);
-    
+
+    require!(
+        flow.is_due_for_execute(now),
+        ErrorCode::JobIsNotDueForExecution
+    );
     let result = do_execute_multisig_flow::handler(&ctx);
     let flow = &mut ctx.accounts.flow;
     flow.update_after_schedule_run(now, true);
-    
-    if !flow.has_remaining_runs() {
-        flow.proposal_stage = ProposalStateType::Complete as u8;
-    }
+
+    flow.proposal_stage = if flow.has_remaining_runs() {
+        ProposalStateType::ExecutionInProgress as u8
+    } else {
+        ProposalStateType::Complete as u8
+    };
 
     result
 }
--- snowflake-rust-v2/programs/snowflake/src/state/approval_record.rs
+++ snowflake-rust-v2/programs/snowflake/src/state/approval_record.rs
@@ -2,7 +2,7 @@ use anchor_lang::prelude::*;
 
 #[derive(AnchorSerialize, AnchorDeserialize, Debug, Clone)]
 pub struct ApprovalRecord {
- pub owner: Pubkey,
- pub date: i64,
- pub is_approved: bool,
+    pub owner: Pubkey,
+    pub date: i64,
+    pub is_approved: bool,
 }
